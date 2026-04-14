7310a8286e3cded72564e9580450f803eec38887
Madhucharan Durgam <durgammadhucharan.131650@marwadiuniversity.ac.in>
2026-04-14 19:30:45 +0530

    Update resumeRoutes.js

--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,16 +1,12 @@
 const express = require("express")
 const router = express.Router()
-
 const upload = require("../services/multerConfig")
 const auth = require("../middleware/authMiddleware")
 
-/* ✅ IMPORT BOTH FUNCTIONS */
 const {
   uploadResume,
   getResumes
 } = require("../controllers/resumeController")
-
-/* ROUTES */
 router.post("/", auth, upload.single("resume"), uploadResume)
 router.get("/", auth, getResumes)
 
