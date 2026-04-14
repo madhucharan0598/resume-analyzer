eabdc269a9d9ab5347d88b416824a5ca2c361b27
Madhucharan Durgam <durgammadhucharan.131650@marwadiuniversity.ac.in>
2026-04-14 19:12:29 +0530

    Update upload.js

--- frontend/js/upload.js
+++ frontend/js/upload.js
@@ -10,7 +10,6 @@ fileInput.addEventListener("change", function () {
   fileName.innerText = uploadedFile.name
 })
 
-/* drag events */
 dropZone.addEventListener("dragover", function (e) {
   e.preventDefault()
   dropZone.classList.add("dragover")
@@ -24,25 +23,17 @@ dropZone.addEventListener("drop", function (e) {
   uploadedFile = e.dataTransfer.files[0]
   fileName.innerText = uploadedFile.name
 })
-
 async function uploadResume(){
-
   if(!uploadedFile){
     alert("Upload file first")
     return
   }
-
   try{
-
     const res = await uploadResumeAPI(uploadedFile)
-
     console.log("Upload success:", res)
-
-    /* 🔥 DELAYED REDIRECT (FIXES YOUR ISSUE) */
     setTimeout(() => {
       window.location.href = "dashboard.html"
     }, 300)
-
   }catch(error){
     console.error(error)
     alert("Upload failed")
