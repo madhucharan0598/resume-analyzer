609c0a7d4d89d07224a3ddad408e35b64fc95354
Madhucharan Durgam <durgammadhucharan.131650@marwadiuniversity.ac.in>
2026-04-14 19:32:21 +0530

    Update multerConfig.js

--- backend/services/multerConfig.js
+++ backend/services/multerConfig.js
@@ -2,7 +2,6 @@ const multer = require("multer")
 const path = require("path")
 
 const storage = multer.diskStorage({
-
   destination: (req, file, cb) => {
     cb(null, "uploads/")
   },
@@ -11,16 +10,11 @@ const storage = multer.diskStorage({
     const uniqueName = Date.now() + path.extname(file.originalname)
     cb(null, uniqueName)
   }
-
 })
 
-/* 🔥 FILE FILTER */
 const fileFilter = (req, file, cb) => {
-
   const allowedTypes = [".pdf", ".docx"]
-
   const ext = path.extname(file.originalname).toLowerCase()
-
   if (allowedTypes.includes(ext)) {
     cb(null, true)
   } else {
@@ -28,7 +22,6 @@ const fileFilter = (req, file, cb) => {
   }
 }
 
-/* 🔥 LIMIT FILE SIZE */
 const upload = multer({
   storage,
   fileFilter,
